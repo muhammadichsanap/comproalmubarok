e8e7afd655ed26fded07bba3ef210f72ebe4f278
ffaizhimawan <faizhimawan26@gmail.com>
2024-06-02 22:56:57 +0700

    menambah kontak kami dan responsif visimisi

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,6 +8,7 @@
 
     <link rel="stylesheet" href="/css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
     @yield('content')
